8ffde3a9451f03f1eac6edecca20b0e1cc0c60fd
Jon <trungt8998@gmail.com>
2019-03-26 10:03:28 -0500

    complete section 290

--- app.js
+++ app.js
@@ -9,9 +9,7 @@ app.set('view engine', 'ejs') //Looking for files with ending .ejs. keep render 
 
 
 var campgrounds = [
-    {"name": "Jungle", "image": "https://pixabay.com/get/e837b1072af4003ed1584d05fb1d4e97e07ee3d21cac104490f3c17ba7edb4b8_340.jpg"},
-    {"name": "Lake", "image": "https://pixabay.com/get/e83db50929f0033ed1584d05fb1d4e97e07ee3d21cac104490f3c17ba7edb4b8_340.jpg"},
-    {"name": "Ocean", "image": "https://pixabay.com/get/e03db50f2af41c22d2524518b7444795ea76e5d004b0144496f0c379a7ecb5_340.jpg"},
+    {"name": "Jungle", "image": "https://pixabay.com/get/e83db7082af3043ed1584d05fb1d4e97e07ee3d21cac104490f3c779afe5b1b9_340.jpg"},
 ]
 
 app.get('/',(req,res)=>{
@@ -31,9 +29,7 @@ app.get('/campgrounds/new', (req,res)=>{
 app.post('/campgrounds', (req,res)=>{
     var name = req.body.name
     var image = req.body.image
-    console.log(name)
     var newCampground = { name : name, image: image}
-    console.log(newCampground)
     campgrounds.push(newCampground)
 
     res.redirect('/campgrounds')
