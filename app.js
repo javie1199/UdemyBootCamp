64f33c2431158361bc78127fdc711911ddafe41a
Jon <trungt8998@gmail.com>
2019-03-27 11:22:01 -0500

    Section 304 campground Show Page part 2

--- app.js
+++ app.js
@@ -9,27 +9,25 @@ mongoose.connect("mongodb://localhost/yelp_camp", { useNewUrlParser: true })
 //SCHEMA SETUP
 var campgroundSchema = new mongoose.Schema({
     name: String,
-    image: String
+    image: String,
+    description: String
 })
 
-var Campground = mongoose.model("Camground", campgroundSchema)
+var Campground = mongoose.model("Campground", campgroundSchema)
 
 // Campground.create(
 //     {
 //         name: "Ocean",
-//         image: "https://pixabay.com/get/e83db7082af3043ed1584d05fb1d4e97e07ee3d21cac104490f3c779afe5b1b9_340.jpg"
+//         image: "https://pixabay.com/get/e83db7082af3043ed1584d05fb1d4e97e07ee3d21cac104490f3c779afe5b1b9_340.jpg",
+//         description: "Beautiful scence and no water, no food, engjoy"
 //     },(err, campground)=>{
 //         if(err){console.log(err)}
 //         else{console.log("Newly created camground")}
 //     }
 // )
+
 app.use(bodyParser.urlencoded({ extended: true }))
 app.set('view engine', 'ejs') //Looking for files with ending .ejs. keep render without .ejs
-
-
-// var campgrounds = [
-//     {"name": "Jungle", "image": "https://pixabay.com/get/e83db7082af3043ed1584d05fb1d4e97e07ee3d21cac104490f3c779afe5b1b9_340.jpg"},
-// ]
 
 app.get('/',(req,res)=>{
     res.render('landing')
@@ -38,24 +36,36 @@ app.get('/',(req,res)=>{
 app.get('/campgrounds',(req,res)=>{
     Campground.find({},function(err, allCampgrounds){
         if(err){console.log(err)}
-        else{res.render('campgrounds',{campgrounds:allCampgrounds})}
+        else{res.render('index',{campgrounds:allCampgrounds})}
     })
 
     
 })
 
+//put get campgrounds/new before campgrounds/:id so that it use /new first
 app.get('/campgrounds/new', (req,res)=>{
     res.render('addCampgrounds')
+})
+
+app.get('/campgrounds/:id',(req, res)=>{
+    console.log(req.params.id) //req.params get info from URL. req.body get infor from POST method
+    Campground.findById(req.params.id,function(err, foundCampground){
+        if(err){console.log(err)}
+        else{
+            res.render('show',{campgrounds: foundCampground})
+        }
+    })
+    
 })
 
 app.post('/campgrounds', (req,res)=>{
     var name = req.body.name
     var image = req.body.image
-    var newCampground = { name : name, image: image}
+    var desc = req.body.description
+    var newCampground = { name : name, image: image, description: desc}
     console.log(newCampground)
     try {
         Campground.create(newCampground)
-        
     } catch (error) {
         console.log(error)
     } finally{ res.redirect('/campgrounds')}
